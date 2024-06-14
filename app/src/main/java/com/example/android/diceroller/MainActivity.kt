5297b0955a12013c5490de96b554c54138f4752b
topouzid <topouzid@yahoo.com>
2024-06-14 12:49:59 +0300

    Replaced findViewById with view Binding. Linear Layout is centered but vertical center (layout_gravity) doesn't work with Binding view, only horizontal layout_gravity. If setContentView(R.layout.activity_main) is used instead of Binding, gravity center works correctly. Work in progress...

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.android.diceroller.databinding.ActivityMainBinding
@@ -11,6 +12,9 @@ import java.util.*
 //The following is used for DataBinding instead of findViewById
 private lateinit var binding: ActivityMainBinding
 
+////The following is normally being used to call findViewById as little times as possible, here used by binding
+//lateinit var diceImage: ImageView
+//
 //class MainActivity : AppCompatActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
@@ -23,13 +27,24 @@ private lateinit var binding: ActivityMainBinding
 //            Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
 //            rollDice()
 //        }
-//
+//        diceImage = findViewById(R.id.dice_image)
 //    }
 //
 //    private fun rollDice() {
 //        val randomInt = Random().nextInt(6) + 1
 //        val resultText: TextView = findViewById(R.id.result_textview)
+//        val resultDiceImage: ImageView = findViewById(R.id.dice_image)
 //        resultText.text = randomInt.toString()
+//        val diceDrawableResource = when (randomInt) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            6 -> R.drawable.dice_6
+//            else -> R.drawable.empty_dice
+//        }
+//        diceImage.setImageResource(diceDrawableResource)
 //    }
 //}
 
@@ -46,7 +61,7 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = binding.diceButton
         rollButton.text = "Let's Roll"
-        binding.resultTextview.text = "Result"
+        binding.resultTextview.text = "Roll that dice!"
 //        binding.resultTextview.setText("Zero?")
 
         rollButton.setOnClickListener {
@@ -58,7 +73,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_textview)
+        val resultText: TextView = binding.resultTextview
+        val resultDiceImage: ImageView = binding.diceImage
         resultText.text = randomInt.toString()
+        val diceDrawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        resultDiceImage.setImageResource(diceDrawableResource)
     }
 }
