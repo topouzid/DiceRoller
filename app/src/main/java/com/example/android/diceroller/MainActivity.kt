11eb31cf7b2d90842edd33bd8634922a47dbf1cb
topouzid <topouzid@yahoo.com>
2024-07-11 11:56:29 +0300

    Added Karlaftopoulos artwork. Removed Landscape orientation. Changed target Sdk to 34 to conform with latest Android rules.
    Replaced findViewById with view Binding.
    Linear Layout is centered but vertical center (layout_gravity) doesn't work with Binding view, only horizontal layout_gravity. If setContentView(R.layout.activity_main) is used instead of Binding, gravity center works correctly.
    Work in progress...

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.android.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android.diceroller.databinding.ActivityMainBinding
-import java.util.*
+import java.util.Random
 
 //The following is used for DataBinding instead of findViewById
 private lateinit var binding: ActivityMainBinding
@@ -77,14 +80,37 @@ class MainActivity : AppCompatActivity() {
         val resultDiceImage: ImageView = binding.diceImage
         resultText.text = randomInt.toString()
         val diceDrawableResource = when(randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
+            1 -> R.drawable.dice_1k
+            2 -> R.drawable.dice_2k
+            3 -> R.drawable.dice_3k
+            4 -> R.drawable.dice_4k
+            5 -> R.drawable.dice_5k
+            6 -> R.drawable.dice_6k
             else -> R.drawable.empty_dice
         }
         resultDiceImage.setImageResource(diceDrawableResource)
     }
+
+    fun onClickOpenPrivacyPolicyButton(view: View) {
+
+        // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https://
+        //val urlAsString = "http://android.tnet.gr/Dice-Roller/"
+        // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
+        //openWebPage(urlAsString)
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://android.tnet.gr/Dice-Roller/")))
+    }
+/*
+    private fun openWebPage(urlAsString: String) {
+
+        // TODO (2) Use Uri.parse to parse the String into a Uri
+        val webpage = Uri.parse(urlAsString)
+
+        // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+
+        // TODO (4) Verify that this Intent can be launched and then call startActivity
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    } */
 }
