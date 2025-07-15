d526fff992481b0632e27feb15e7c4196a0bc737
topouzid <topouzid@yahoo.com>
2025-07-15 21:29:02 +0300

    Added Greek, Changed to 36, modernized code, enabled edge-to-edge for latest specs. Added Landscape Orientation again, with Double Dice on Landscape. Added Karlaftopoulos artwork.
    Replaced findViewById with view Binding.
    Linear Layout is centered but vertical center (layout_gravity) doesn't work with Binding view, only horizontal layout_gravity. If setContentView(R.layout.activity_main) is used instead of Binding, gravity center works correctly.
    Work in progress...
    
    36

--- app/src/main/java/gr/tnet/android/diceroller/MainActivity.kt
+++ app/src/main/java/gr/tnet/android/diceroller/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.android.diceroller
+/*package gr.tnet.android.diceroller
 
 import android.content.Intent
 import android.net.Uri
@@ -7,49 +7,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+//import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android.diceroller.databinding.ActivityMainBinding
+import gr.tnet.android.diceroller.databinding.ActivityMainBinding
 import java.util.Random
 
 //The following is used for DataBinding instead of findViewById
 private lateinit var binding: ActivityMainBinding
-
-////The following is normally being used to call findViewById as little times as possible, here used by binding
-//lateinit var diceImage: ImageView
-//
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        val rollButton: Button = findViewById(R.id.dice_button)
-//        rollButton.text = "Let's Roll"
-//
-//        rollButton.setOnClickListener {
-//            Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
-//            rollDice()
-//        }
-//        diceImage = findViewById(R.id.dice_image)
-//    }
-//
-//    private fun rollDice() {
-//        val randomInt = Random().nextInt(6) + 1
-//        val resultText: TextView = findViewById(R.id.result_textview)
-//        val resultDiceImage: ImageView = findViewById(R.id.dice_image)
-//        resultText.text = randomInt.toString()
-//        val diceDrawableResource = when (randomInt) {
-//            1 -> R.drawable.dice_1
-//            2 -> R.drawable.dice_2
-//            3 -> R.drawable.dice_3
-//            4 -> R.drawable.dice_4
-//            5 -> R.drawable.dice_5
-//            6 -> R.drawable.dice_6
-//            else -> R.drawable.empty_dice
-//        }
-//        diceImage.setImageResource(diceDrawableResource)
-//    }
-//}
 
 
 class MainActivity : AppCompatActivity() {
@@ -66,7 +30,6 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button? = binding.diceButton
         rollButton?.text = "Let's Roll"
         binding.resultTextview?.text = "Roll that dice!"
-//        binding.resultTextview.setText("Zero?")
 
         rollButton?.setOnClickListener {
 //            Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
@@ -140,18 +103,96 @@ class MainActivity : AppCompatActivity() {
         //openWebPage(urlAsString)
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://android.tnet.gr/Dice-Roller/")))
     }
-/*
-    private fun openWebPage(urlAsString: String) {
 
-        // TODO (2) Use Uri.parse to parse the String into a Uri
-        val webpage = Uri.parse(urlAsString)
+}
+*/
 
-        // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
+package gr.tnet.android.diceroller
 
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
+import android.content.Intent
+//import android.net.Uri
+import androidx.core.net.toUri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import gr.tnet.android.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
+import androidx.activity.enableEdgeToEdge //edge to edge
+//import androidx.core.view.WindowCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Enable edge-to-edge support for Android 15+ (and all supported versions)
+        //WindowCompat.enableEdgeToEdge(window)
+        enableEdgeToEdge()
+
+        // Creating a binding object for the main_activity.xml layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.diceButton?.text = getString(R.string.diceBtnString)
+        binding.diceButton?.setOnClickListener { rollDice() }
+        binding.resultTextview?.text = getString(R.string.diceString)
+
+        // If you have a double-dice feature
+        binding.diceButtonDouble?.text = getString(R.string.diceDblBtnString)
+        binding.diceButtonDouble?.setOnClickListener { rollDiceDouble() }
+        binding.resultTextviewDouble1?.text = getString(R.string.diceString)
+        binding.resultTextviewDouble2?.text = "..."
+
+        binding.privacypolicyButtonview?.setOnClickListener {
+            openPrivacyPolicy()
         }
-    } */
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(1, 7) // 1..6 inclusive
+        binding.resultTextview?.text = randomInt.toString()
+        val diceDrawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1k
+            2 -> R.drawable.dice_2k
+            3 -> R.drawable.dice_3k
+            4 -> R.drawable.dice_4k
+            5 -> R.drawable.dice_5k
+            6 -> R.drawable.dice_6k
+            else -> R.drawable.empty_dice
+        }
+        binding.diceImage?.setImageResource(diceDrawableResource)
+    }
+
+    private fun rollDiceDouble() {
+        val randomInt1 = Random.nextInt(1, 7)
+        val randomInt2 = Random.nextInt(1, 7)
+        binding.resultTextviewDouble1?.text = randomInt1.toString()
+        binding.resultTextviewDouble2?.text = randomInt2.toString()
+        val diceDrawableResource1 = when (randomInt1) {
+            1 -> R.drawable.dice_1k
+            2 -> R.drawable.dice_2k
+            3 -> R.drawable.dice_3k
+            4 -> R.drawable.dice_4k
+            5 -> R.drawable.dice_5k
+            6 -> R.drawable.dice_6k
+            else -> R.drawable.empty_dice
+        }
+        val diceDrawableResource2 = when (randomInt2) {
+            1 -> R.drawable.dice_1k
+            2 -> R.drawable.dice_2k
+            3 -> R.drawable.dice_3k
+            4 -> R.drawable.dice_4k
+            5 -> R.drawable.dice_5k
+            6 -> R.drawable.dice_6k
+            else -> R.drawable.empty_dice
+        }
+        binding.diceImageDouble1?.setImageResource(diceDrawableResource1)
+        binding.diceImageDouble2?.setImageResource(diceDrawableResource2)
+    }
+
+    fun openPrivacyPolicy() {
+        //val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://android.tnet.gr/Dice-Roller/"))
+        val intent = Intent(Intent.ACTION_VIEW, "http://android.tnet.gr/Dice-Roller/".toUri())
+        startActivity(intent)
+    }
 }
